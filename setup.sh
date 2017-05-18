45f86fd8d31f34f953c431a1f391260772bcf96e
Rajeev Thakur <rajeev@letsflock.mobi>
2017-05-18 09:49:53 -0700

    update for new libs

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -x
 mkdir DAQ
 cd DAQ
-curl -O http://pocoproject.org/releases/poco-1.7.3/poco-1.7.3.tar.gz
+curl -O https://pocoproject.org/releases/poco-1.7.3/poco-1.7.3.tar.gz
 tar zxvf poco-1.7.3.tar.gz
 cd poco-1.7.3
 ./configure --omit=Data/ODBC,Data/MySQL,NetSSL_OpenSSL,Crypto
 make
 make install
 cd ..
-wget http://sourceforge.net/projects/arma/files/armadillo-6.700.6.tar.gz
-tar zxvf armadillo-6.700.6.tar.gz
-cd armadillo-6.700.6
+
+wget http://sourceforge.net/projects/arma/files/armadillo-7.900.1.tar.xz
+tar zxvf armadillo-7.900.1.tar.xz
+cd armadillo-7.900.1
 cmake .
 make
 make install
